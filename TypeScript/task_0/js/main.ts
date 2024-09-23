bee69dab51604ca0340c70a956d8cefe0d5a8754
SergioLebron22 <lebronsergio22@gmail.com>
2024-09-23 10:06:19 -0400

    Added task 0

---
+++ TypeScript/task_0/js/main.ts
@@ -0,0 +1,11 @@
+interface Student {
+    firstName: string,
+    lastName: string,
+    age: number,
+    location: string,
+}
+
+let student1: Student;
+let student2: Student;
+
+let studentList: Student[] = [student1, student2]
