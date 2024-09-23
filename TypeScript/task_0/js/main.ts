abf79f97fbf2237282a322e4f99b397ebf85e35a
SergioLebron22 <lebronsergio22@gmail.com>
2024-09-23 15:24:33 -0400

    fixed ts file in task 0, added table code

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -9,3 +9,24 @@ let student1: Student;
 let student2: Student;
 
 let studentList: Student[] = [student1, student2]
+
+const table = document.createElement('table');
+const tableBody = document.createElement('tbody');
+
+studentList.forEach(student => {
+    const row = document.createElement('tr');
+
+    const fisrtNameCell = document.createElement('td');
+    fisrtNameCell.textContent = student.firstName;
+    row.appendChild(fisrtNameCell);
+
+    const locationCell = document.createElement('td');
+    locationCell.textContent = student.location;
+    row.appendChild(locationCell);
+
+    tableBody.appendChild(row);
+});
+
+table.appendChild(tableBody);
+
+document.body.appendChild(table);
